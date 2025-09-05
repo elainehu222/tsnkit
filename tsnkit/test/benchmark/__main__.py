016718ff94171ccbceab29a1116ef11fc0b487e8
elaine <elainehu222@gmail.com>
2025-09-04 20:43:00 -0400

    process order: model, task (reversed) no stop between models

--- tsnkit/test/benchmark/__main__.py
+++ tsnkit/test/benchmark/__main__.py
@@ -122,6 +122,7 @@ if __name__ == "__main__":
     oom.start()
 
     def store(output, verbose=True):
+        print("callback")
         # output = [task, result, algo_time, total_time, algo_mem, total_mem]
         flag = output[1]
         _task = output[0]
@@ -141,10 +142,11 @@ if __name__ == "__main__":
     def run(alg, task_param: str, workers: int):
         print(f"running {task_param}")
         processes[os.getpid()] = task_param
+        print(os.getpid())
         task_num = task[1]
         path = SCRIPT_DIR + "/data/" + task_num
         stats = alg(f"{task_param[0]}-{task_num}", path + "_task.csv", path + "_topo.csv", workers=workers)
-
+        print("succ")
         return stats.to_list()
 
     with Pool(processes=cpu_count() // utils.NUM_CORE_LIMIT, maxtasksperchild=1, initializer=mute) as p:
