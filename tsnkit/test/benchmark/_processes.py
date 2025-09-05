9785413dca8ee4bc8d98a8f3e1a7e4ae1b31f267
elaine <elainehu222@gmail.com>
2025-09-04 21:07:29 -0400

    process order: model, task (reversed) no stop between models

--- tsnkit/test/benchmark/_processes.py
+++ tsnkit/test/benchmark/_processes.py
@@ -11,7 +11,6 @@ from tqdm import tqdm
 
 from ...core import Result
 from ...simulation import tas
-from ...algorithms import dt
 
 SCRIPT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
@@ -112,6 +111,6 @@ def run(alg, task_param: str, workers: int):
     # print(os.getpid())
     task_num = task_param[1]
     path = SCRIPT_DIR + "/data/" + task_num
-    stats = dt.benchmark(f"{task_param[0]}-{task_num}", path + "_task.csv", path + "_topo.csv", workers=workers)
+    stats = alg(f"{task_param[0]}-{task_num}", path + "_task.csv", path + "_topo.csv")
     print("succ")
     return stats.to_list()
