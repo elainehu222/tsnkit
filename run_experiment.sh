ebc6a177da6944901237cc2378839e387a087cd1
elaine <elainehu222@gmail.com>
2025-10-02 11:25:08 -0400

    Update run_experiment.sh

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+export PATH=$PATH:/opt/ibm/ILOG/CPLEX_Studio2211/cpoptimizer/bin/x86-64_linux
+export GRB_LICENSE_FILE="/opt/gurobi1203/gurobi.lic"
+
 rm -rf ./out && mkdir ./out
 
 source /home/cc/Python-3.13.7/venv/bin/activate
 
-python3 -m tsnkit.tsnkit.test.benchmark --methods ALL --ins 1-256 -t 7200 -o ./results/
+nohup python3 -m tsnkit.tsnkit.test.benchmark --methods ALL --ins 1-256 -t 7200 -o ./results/
 
 # This file is loaded automatically onto nodes
 source ~/openrc
