5b843ce26d1fdc270e5c808168c89cb63a3b16b2
elaine <elainehu222@gmail.com>
2025-10-02 11:36:12 -0400

    Update run_experiment.sh

--- run_experiment.sh
+++ run_experiment.sh
@@ -7,6 +7,8 @@ rm -rf ./out && mkdir ./out
 
 source /home/cc/Python-3.13.7/venv/bin/activate
 
+pip install python-swiftclient
+
 nohup python3 -m tsnkit.tsnkit.test.benchmark --methods ALL --ins 1-256 -t 7200 -o ./results/
 
 # This file is loaded automatically onto nodes
@@ -14,14 +16,12 @@ source ~/openrc
 
 today=$(date '+%Y-%m-%d')
 bucket_name="tsnkit-benchmark_$today"
+openstack container create $bucket_name
 
-echo
 echo "Uploading results to the object store container $bucket_name"
-# Create the bucket if it doesn't exist
-swift post $bucket_name
 
 for file in ./results/*; do
 	echo "Uploading $file"
-	swift upload $bucket_name $file --object-name $(basename $file)
+	openstack object create $bucket_name $file
 done
 
