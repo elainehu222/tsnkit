b164eff0ea888e25ce36cd7af52ed808250cabec
elaine <elainehu222@gmail.com>
2025-10-06 15:12:00 -0400

    Update run_experiment.sh

--- run_experiment.sh
+++ run_experiment.sh
@@ -9,7 +9,7 @@ source /home/cc/Python-3.13.7/venv/bin/activate
 
 pip install python-swiftclient
 
-nohup python3 -m tsnkit.tsnkit.test.benchmark --methods ALL --ins 1-256 -t 7200 -o ./results/
+nohup python3 -m tsnkit.test.benchmark --methods ALL --ins 1-256 -t 7200 -o ./results/
 
 # This file is loaded automatically onto nodes
 source ~/openrc
