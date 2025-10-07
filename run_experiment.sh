c2a2ea19385f9c1db68d48b61ee32585cc357067
elaine <elainehu222@gmail.com>
2025-10-07 09:33:34 -0400

    Update run_experiment.sh

--- run_experiment.sh
+++ run_experiment.sh
@@ -3,13 +3,13 @@
 export PATH=$PATH:/opt/ibm/ILOG/CPLEX_Studio2211/cpoptimizer/bin/x86-64_linux
 export GRB_LICENSE_FILE="/opt/gurobi1203/gurobi.lic"
 
-rm -rf ./out && mkdir ./out
+rm -rf ./results && mkdir ./results
 
 source /home/cc/Python-3.13.7/venv/bin/activate
 
 pip install python-swiftclient
 
-nohup python3 -m tsnkit.test.benchmark --methods ALL --ins 1-256 -t 7200 -o ./results/
+nohup python3 -m tsnkit.tsnkit.test.benchmark --methods ALL --ins 1-256 -t 7200 -o ./results/
 
 # This file is loaded automatically onto nodes
 source ~/openrc
